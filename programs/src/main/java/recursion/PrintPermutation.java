8ea78f37ba59e97bfc0a2ae7bc6aa4b358796040
sathishd206 <sathishd189@gmail.com>
2024-05-02 12:42:16 +0530

    print permutation

--- programs/src/main/java/recursion/PrintPermutation.java
+++ programs/src/main/java/recursion/PrintPermutation.java
@@ -2,4 +2,36 @@ package recursion;
 
 public class PrintPermutation {
 
+	public static void main(String[] args) {
+		String s="ABCD";
+		int n=s.length();
+		permute(s,0,n-1);
+	}
+	
+	/*
+	 * l--first index, r--last index
+	 */
+	public static void permute(String s, int l,int r) {
+		if(l==r) {
+			System.out.println(s);//base case
+		}
+		else {
+			for(int i=l;i<=r;i++) {
+			s=swap(s,l,i);//root to leaf
+			permute(s,l+1,r);
+			s=swap(s,l,i);	// backtrack
+			}
+		}
+	}
+	public static String swap(String a,int i,int j) {
+		char temp;
+		char[] charArray=a.toCharArray();
+		temp=charArray[i];
+		charArray[i]=charArray[j];
+		charArray[j]=temp;
+		return String.valueOf(charArray);
+	}
+	
 }
+
+
